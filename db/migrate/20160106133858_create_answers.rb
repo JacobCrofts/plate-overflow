a62ec3f1202ece3ce9fe97b010c2eb037e6e547d
Jacob Crofts <email": null>
2016-01-06 13:48:53 -0600

    Complete migration files

--- db/migrate/20160106133858_create_answers.rb
+++ db/migrate/20160106133858_create_answers.rb
@@ -1,4 +1,12 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
+    create_table :answers do |t|
+      t.text :content, null: false
+      t.integer :author_id, null: false
+      t.boolean :is_best_answer, default: false
+      t.integer :question_id, null: false
+
+      t.timestamps
+    end
   end
 end
