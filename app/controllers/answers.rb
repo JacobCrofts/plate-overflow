813759f18376e03826c734846a503ce6d9b0df68
beckylehmann91 <beckylehmann91@gmail.com>
2016-01-07 13:29:14 -0600

    Update views, controllers and js for edit answers

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,6 +1,6 @@
 get '/questions/:question_id/answers' do
   @answers = Answer.where(question_id: params[:question_id])
-  erb :"answers/index"
+  # erb :"answers/index"
 end
 
 get '/questions/:question_id/answers/new' do
@@ -17,5 +17,15 @@ end
 get '/questions/:question_id/answers/:answer_id/edit' do
   @question = Question.find(params[:question_id])
   @answer = Answer.find(params[:answer_id])
-  erb :"answers/edit"
+  # erb :"answers/edit"
+end
+
+put '/questions/:question_id/answers/:answer_id' do
+  @question = Question.find(params[:question_id])
+  @answer.update_attributes(params[:question])
+  if @question.save
+    redirect '/questions'
+  else
+    erb :errors
+  end
 end
---
+++ public/js/answers.js
@@ -0,0 +1,67 @@
+$(function(){
+  // when button click load form for comment and author
+  $(show_answer_form());
+    $(add_answer_data());
+});
+
+$(function(){
+  $(show_edit_form());
+    // $(update_answer_data());
+});
+
+function show_edit_form(){
+  $("#edit_answer_button").on("click", function(event){
+      event.preventDefault();
+      $("#edit_answer_button").hide()
+      $("#edit_answer_form").show()
+  });
+};
+
+function show_answer_form(){
+  $("#new_answer_button").on("click", function(event){
+    event.preventDefault();
+    $("#new_answer_button").hide()
+    $("#new_answer_form").show()
+  });
+};
+
+function add_answer_data(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var url = $(this).attr("action");
+    var data = $(this).serialize();
+    var request = $.ajax({
+      method: "POST",
+      url: url,
+      data: data
+      // missing toggle of comment box
+    });
+    request.done(function(body) {
+      var parsed_data = JSON.parse(body);
+      append_new_answer(parsed_data);
+    })
+  });
+};
+
+function update_answer_data(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var url = $(this).attr("action");
+    var data = $(this).serialize();
+    var request = $.ajax({
+      method: "POST",
+      url: url,
+      data: data
+      // missing toggle of comment box
+    });
+    request.done(function(body) {
+      var parsed_data = JSON.parse(body);
+      append_new_answer(parsed_data);
+    })
+  });
+};
+
+function append_new_answer(parsed_data){
+  $(".answer_list").append("<li>" + parsed_data.answer + "</li>");
+  $(".answer_list li:last-child").append("<span class=\"author\">" + parsed_data.author + "</span>");
+};
