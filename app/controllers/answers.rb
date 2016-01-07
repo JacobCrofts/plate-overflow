1fd368f7d2a664d2fb4ef85143e533e3f6908da0
beckylehmann91 <beckylehmann91@gmail.com>
2016-01-07 12:05:05 -0600

    Create partials for answer index, edit and new

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -4,7 +4,7 @@ get '/questions/:question_id/answers' do
 end
 
 get '/questions/:question_id/answers/new' do
-  erb :"answers/new"
+  # erb :"answers/new"
 end
 
 post '/questions/:question_id/answers' do
@@ -12,4 +12,10 @@ post '/questions/:question_id/answers' do
   @author = User.find(session[:user_id])
   @answer = @question.answers.create(content: params[:answer], author_id: @author.id)
   {answer: @answer.content, author: @author.username}.to_json
+end
+
+get '/questions/:question_id/answers/:answer_id/edit' do
+  @question = Question.find(params[:question_id])
+  @answer = Answer.find(params[:answer_id])
+  erb :"answers/edit"
 end
