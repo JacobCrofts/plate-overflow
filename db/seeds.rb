697503c2977cfb1a5e27f66ed9af745d83b12f53
Pratik Shah <email": null>
2016-01-08 07:59:21 -0600

    add seed data content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 names = ["Tyrion", "Melisandre", "Brienne", "Eddard"]
 emails = ["tyrionlannister@gmail.com", "rhloorisbae@gmail.com", "fuckstannis@gmail.com", "winteriscoming@gmail.com"]
 
-question_titles = ["question 1", "question 2", "question 3", "question 4"]
-question_contents = ["q1 content", "q2 content", "q3 content", "q4 content"]
+question_titles = ["Spork -vs- Fork", "Tacos", "Important", "Book"]
+question_contents = ["All right guys. I know this is a bit of a hot button issue, but I have to know. Which utensil is truly better: the spork or the fork?", "Anyone have a great recipe for tacos they would like to share", "Who is the one true chef of Westeros?", "When is George R.R. Martin finishing his next cookbook?"]
 
-answer_content = ["answer 1", "answer 2", "answer 3", "answer 4"]
-comment_content = ["comment 1", "comment 2", "comment 3", "comment 4"]
+answer_content = ["Well there are two camps of people in this. Those who believe forks are better, and those who are wrong.", "Nah.", "Stannis is the one true chef.", "Probably not any time soon. His cooking show is actually fully caught up with his recipe books. I'm not sure what they're going to do from this point on..."]
+comment_content = ["Well this escalated quickly...", "Thank you so much.", "Joffrey is the one true chef!", "I'll be waiting..."]
 
 (0..3).each do |i|
   User.create(username: names[i], password: "password", email: emails[i])
